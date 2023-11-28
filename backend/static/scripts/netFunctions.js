f9a9c53accec69bf28cb7155cefe498c414b1a96
MrNightFury <73826973+MrNightFury@users.noreply.github.com>
2023-11-28 20:38:30 +0300

    Day 2.5

---
+++ backend/static/scripts/netFunctions.js
@@ -0,0 +1,23 @@
+document.getElementById("loginForm").addEventListener("submit", event => {
+    event.preventDefault();
+    document.querySelector("#loginInput label").style.display = "none";
+    document.querySelector("#passwordInput label").style.display = "none";
+    let formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => (data[key] = value));
+    fetch("/auth/login", {body: JSON.stringify(data), method: "POST", headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      }}).then(async result => {
+        if (result.status == 404) {
+            document.querySelector("#loginInput label").style.display = "inline";
+        } else if (result.status == 401) {
+            document.querySelector("#passwordInput label").style.display = "inline";
+        } else {
+            location.href = "/account/" + data.login;
+        }
+    })
+})
+
+function login() {
+    
+}
